2d693db65e7faa5884818109a8296ea18c0d5e2a
Işıl Ç. <93406894+zeynepisilcimen@users.noreply.github.com>
2024-08-29 15:16:45 +0300

    Create config.h
    App-Core-Inc- config.h

---
+++ Config.h
@@ -0,0 +1,22 @@
+/*
+ * config.h
+ *
+ *  Created on: Aug 27, 2024
+ *      Author: ISIL-CIMEN
+ */
+
+#ifndef INC_CONFIG_H_
+#define INC_CONFIG_H_
+
+// Header dosyasındaki tanımlar ve prototipler buraya gelir
+// Örneğin:
+#define CONFIG_PARAMETER 10
+
+void configFunction(void);
+
+#endif /* INC_CONFIG_H_ */
+
+#ifndef INCLUDE_SCHA63TK01
+#define INCLUDE_SCHA63TK01
+
+#endif /* INC_CONFIG_H_ */
